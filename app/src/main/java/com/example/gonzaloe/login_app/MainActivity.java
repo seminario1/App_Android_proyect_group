68ab32710e7ce2e7ab1a2553211cf2d6997d8c99
Gonzalo espinpza <gonis1990@gmail.com>
2018-05-25 19:37:45 -0400

    se impelemto el login con google, y el registro de usuarios..

--- app/src/main/java/com/example/gonzaloe/login_app/MainActivity.java
+++ app/src/main/java/com/example/gonzaloe/login_app/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.gonzaloe.login_app;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -9,7 +13,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -19,107 +28,111 @@ import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private EditText Name;
     private  EditText Password;
     private TextView  Info;
     private Button Login;
+    //thirActivity
+    private Button Register;
     private String eemail2;
-    private int counter = 5;
+    private String pw;
+
+    GoogleApiClient client;
+    @TargetApi(Build.VERSION_CODES.M)
+    @RequiresApi(api = Build.VERSION_CODES.M)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Name = (EditText)findViewById(R.id.etName);
-        Password = (EditText)findViewById(R.id.etPassword);
-        Info = (TextView)findViewById(R.id.tvInfo);
-        Login = (Button)findViewById(R.id.btnLogin);
 
+        //gogle client
+        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        client = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, options)
+                .build();
+
+        Name = findViewById(R.id.etName);
+        Password = findViewById(R.id.etPassword);
+        Login = findViewById(R.id.btnLogin);
+        Register = findViewById(R.id.btnRegister); //button activity thirdActivity
 
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validate(Name.getText().toString(), Password.getText().toString());
+
+                loadInitialRestData(Name.getText().toString(), Password.getText().toString());
+
             }
 
         });
 
-        //loadInitialRestData();
-        loadComponents();
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                startActivity(intent);
+
+
+
+            }
+        });
+
     }
 
 
+    private void loadInitialRestData(String email, String pass) {
 
-    private void loadInitialRestData(String str) {
-        AsyncHttpClient client =new AsyncHttpClient();
-        client.get("http://192.168.1.3:4030/login/"+str+"=244", new JsonHttpResponseHandler(){
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://192.168.1.3:4030/login/"+email+"="+pass, new JsonHttpResponseHandler(){
 
-                    @Override
-                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                        try {
-                          JSONArray list =  (JSONArray)response.get("email");
-                          for (int i = 0; i < list.length();i++){
-                              JSONObject itenJosn =list.getJSONObject(i);
-                              String email = itenJosn.getString("email");
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
 
-                              eemail2 = email;
-                          }
+                try {
 
+                    JSONArray list =  (JSONArray)response.get("email");
+                    for (int i = 0; i < list.length();i++){
+                        JSONObject itenJosn =list.getJSONObject(i);
+                        String email1 = itenJosn.getString("email");
+                        String pssw = itenJosn.getString("password");
+
+                        Toast.makeText(getApplicationContext(),"Welcome", Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                            startActivity(intent);
+                            break;
+
+                        }
 
                         } catch (JSONException e) {
                             e.printStackTrace();
+
                         }
                     }
                     @Override
-                    public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse)  {
+                        Toast toast1 = Toast.makeText(getApplicationContext(), "verify your emil or password", Toast.LENGTH_SHORT);
+                        toast1.show();
                     }
+
+
         });
 
+        Password.setText("");
     }
 
 
-    //
-    private void loadComponents() {
+    //login google
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
-        EditText mail = (EditText) this.findViewById(R.id.etName);
-        EditText pass = (EditText)this.findViewById(R.id.etPassword);
-
-        //events
-        mail.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
-                String str = s.toString();
-                loadInitialRestData(str);
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
-    }
-
-
-
-
-    private void validate( String userName, String userPassword) {
-        if (userName.equals(eemail2) && userPassword.equals("123")){
-            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            startActivity(intent);
-
-        }else{
-            counter --;
-            if (counter==0){
-                Login.setEnabled(false);
-            }
-        }
     }
 }
---
+++ app/src/main/java/com/example/gonzaloe/login_app/ThirdActivity.java
@@ -0,0 +1,114 @@
+package com.example.gonzaloe.login_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.BaseJsonHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText LastName;
+    private EditText Phone;
+    private EditText Email;
+    private EditText Password;
+    private Button Register;
+
+    private int confir=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        Name = findViewById(R.id.etName);
+        LastName = findViewById(R.id.etLastNmae);
+        Phone = findViewById(R.id.etPhone);
+        Email = findViewById(R.id.etEmail);
+        Password = findViewById(R.id.etPassword);
+        Register = findViewById(R.id.btnRegister);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                sendDataRegister(Name.getText().toString(), LastName.getText().toString(), Phone.getText().toString(),
+                            Email.getText().toString(), Password.getText().toString());
+
+                if(confir==1) {
+
+//                    Intent intent = new Intent(ThirdActivity.this, SecondActivity.class);
+//                    startActivity(intent);
+
+                    Name.setText("");
+                    LastName.setText("");
+                    Phone.setText("");
+                    Email.setText("");
+                    Password.setText("");
+
+                }
+            }
+
+        });
+
+    }
+
+    private void sendDataRegister(final String name, String Lastname, String Phone, String Email, String Password) {
+
+        //Toast.makeText(getApplicationContext(),name, Toast.LENGTH_SHORT).show();
+
+
+        if(!name.isEmpty()&&!Lastname.isEmpty()&& !Phone.isEmpty()&& !Email.isEmpty()&& !Password.isEmpty()) {
+
+            confir=1;
+
+            RequestParams params = new RequestParams();
+            params.put("name", name);
+            params.put("lastname", Lastname);
+            params.put("phone", Phone);
+            params.put("email", Email);
+            params.put("password", Password);
+
+
+
+            AsyncHttpClient Client = new AsyncHttpClient();
+            Client.post("http://192.168.1.3:4030/registro", params, new JsonHttpResponseHandler() {
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    try {
+
+                        Toast.makeText(getApplicationContext(), response.getString("name"), Toast.LENGTH_SHORT).show();
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                @Override
+                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+
+                    Toast.makeText(getApplicationContext(),"Usuario ya existente",Toast.LENGTH_SHORT);
+                }
+            });
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"fill in the empty spaces", Toast.LENGTH_SHORT).show();
+            confir=0;
+
+        }
+    }
+
+}
