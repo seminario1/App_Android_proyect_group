0e6856521454b68d1fda97062b3babdba1b3033e
Gonzalo espinpza <gonis1990@gmail.com>
2018-05-26 18:20:23 -0400

    se modifico la venta de registro de inmuebles

--- app/src/main/java/com/example/gonzaloe/login_app/RegistroInmuebles.java
+++ app/src/main/java/com/example/gonzaloe/login_app/RegistroInmuebles.java
@@ -2,15 +2,27 @@ package com.example.gonzaloe.login_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spanned;
+import android.util.SparseIntArray;
+import android.widget.Button;
 
 public class RegistroInmuebles extends AppCompatActivity {
 
 
+
+    private Spanned elegirCasa;
+    private Spanned EstadoCasa;
+
+
+
+    private Button EnviarRegistro;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_inmuebles);
+
+
     }
 }
